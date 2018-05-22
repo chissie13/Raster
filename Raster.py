4cb65a3a8f14976883ad136d46f937133f6dfc22
Chris Markus <0923272@hr.nl>
2018-05-22 13:56:03 +0200

    veel

--- Raster.py
+++ Raster.py
@@ -7,11 +7,14 @@ def add(x, y):
 g = Grid(20, 20)
 def rasterline(x1 , y1, x2, y2):
     if x1>x2:
-        xtemp = x1-x2
-        dirx = "Left"
-    else:
-        xtemp = x2-x1
-        dirx = "Right"
+        temp = x1
+        x1 = x2
+        x2 = temp
+        temp = y1
+        y1 = y2
+        y2 = temp
+    xtemp = x2-x1
+
     if y1>y2:
         ytemp = y1-y2
         diry = "Down"
@@ -32,30 +35,31 @@ def rasterline(x1 , y1, x2, y2):
     anglex = 90 - angle
     anglexTot = 0
     hasGonex = False
-    if dirx == "Right":
-        angle /= 45
-        anglex /= 45
-        print("angle: ", angle)
-        for i in range(0, lineLength):
-            add(x, y)
-            if angle <= 1:
+    angle /= 45
+    anglex /= 45
+    print("angle: ", angle)
+    for i in range(0, lineLength):
+        add(x, y)
+        if angle <= 1:
+            x += 1
+        else:
+            anglexTot += anglex
+            if anglexTot > 0.5 and hasGonex == False:
                 x += 1
-            else:
-                anglexTot += anglex
-                if anglexTot > 0.5 and hasGonex == False:
-                    x += 1
-                    hasGonex = True
-                if anglexTot >= 1:
-                    anglexTot -= 1
-                    hasGonex = False
-            angleTot += angle
-            if angleTot > 0.5 and hasGone == False:
-                y += 1
-                hasGone = True
-            if angleTot >= 1:
-                angleTot -= 1
-                hasGone = False
-            print("AnglexTot: ", anglexTot, "hasGonex: ", hasGonex)
+                hasGonex = True
+            if anglexTot >= 1:
+                anglexTot -= 1
+                hasGonex = False
+        angleTot += angle
+        if angleTot > 0.5 and hasGone == False:
+            if diry == "Down":
+                y -= 2
+            y += 1
+            hasGone = True
+        if angleTot >= 1:
+            angleTot -= 1
+            hasGone = False
+        print("AnglexTot: ", anglexTot, "hasGonex: ", hasGonex)
 
-rasterline(0, 0, 3, 10)
+rasterline(0, 6, 10, 0)
 g.draw()
