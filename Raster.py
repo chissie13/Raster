b0157c2e0b5a765061761995f2e23705715f00b9
Chris Markus <0923272@hr.nl>
2018-05-22 14:51:32 +0200

    alles gefixt

--- Raster.py
+++ Raster.py
@@ -6,7 +6,7 @@ def add(x, y):                  #easier function to add a point
 
 g = Grid(20, 20)                # the grid dimensions (x,y)
 
-def rasterline(x1 , y1, x2, y2):#function to calculate where the points have to go and calls add function accordingly
+def rasterline(x1 , y1, x2, y2):#function to calculate where the points have to go and calls add function accordingly (x1, y1, x2, y2)
     if x1>x2:                   #checks to make sure x1 is on the left side of x2 and changes them if not true
         temp = x1
         x1 = x2
@@ -14,8 +14,10 @@ def rasterline(x1 , y1, x2, y2):#function to calculate where the points have to 
         temp = y1
         y1 = y2
         y2 = temp
+
     xtemp = x2 - x1
 
+    dir_y = 0
     if y1>y2:                   #checks to see if y1 is above or below y2
         ytemp = y1-y2
         dir_y = "Down"
@@ -37,34 +39,38 @@ def rasterline(x1 , y1, x2, y2):#function to calculate where the points have to 
     anglex = 90 - angle                           #angle of line for calculations for the x value
     anglex_tot = 0
     has_gonex = False
-    angle /= 45                                     #casts the degrees to a value between 0-1
-    anglex /= 45
 
     for i in range(0, line_length):
         add(x, y)
 
-        if angle <= 1:                            #checks if the degree is more or less than 45
+        if angle <= 45:                            #checks if the degree is more or less than 45
             x += 1
         else:
             anglex_tot += anglex                    #adds to total to see how far the line is in correlation to the gridsquares
-            if anglex_tot > 0.5 and has_gonex == False:
+            if anglex_tot > 22.5 and has_gonex == False:
                 x += 1
                 has_gonex = True
-            if anglex_tot >= 1:
-                anglex_tot -= 1
+            if anglex_tot >= 45:
+                anglex_tot -= 45
                 has_gonex = False
 
         angle_tot += angle
 
-        if angle_tot > 0.5 and has_gone == False:
+        if angle_tot > 22.5 and has_gone == False:
             if dir_y == "Down":
                 y -= 2
             y += 1
             has_gone = True
 
-        if angle_tot >= 1:
-            angle_tot -= 1
+        if angle_tot >= 45:
+            angle_tot -= 45
             has_gone = False
 
-rasterline(0, 10, 0, 0)                            #testdata :)
+rasterline(10, 10, 0, 0)                            #testdata :)
+rasterline(2, 0, 12, 10)
+rasterline(0, 10, 10, 0)
+rasterline(12, 0, 0, 12)
+rasterline(0, 0, 15, 0)
+rasterline(0, 0, 0, 15)
+
 g.draw()
