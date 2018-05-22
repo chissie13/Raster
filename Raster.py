dba05b6d5a854fa7f5b230c71992219345ececdd
Chris Markus <0923272@hr.nl>
2018-05-22 13:08:33 +0200

    veel

--- Raster.py
+++ Raster.py
@@ -8,51 +8,42 @@ g = Grid(20, 10)
 def rasterline(x1 , y1, x2, y2):
     if x1>x2:
         xtemp = x1-x2
-        dirx = "L"
+        dirx = "Left"
     else:
         xtemp = x2-x1
-        dirx = "R"
+        dirx = "Right"
     if y1>y2:
         ytemp = y1-y2
-        diry = "L"
+        diry = "Down"
     else:
         ytemp = y2-y1
-        diry = "H"
+        diry = "Up"
     if xtemp>ytemp:
         lineLength = xtemp
     else:
         lineLength = ytemp
-
+    print("LineLength",lineLength)
     angle = math.degrees((math.atan(ytemp/xtemp)))
     angleTot = 0
     bonus = 0
     x = x1
     y = y1
-    if dirx == "R" and angle<=45:
-        print (angle)
+    hasGone = False
+    if dirx == "Right" and angle<=45:
         angle /= 45
-        print("angle: ",angle)
+        print("angle: ", angle)
         for i in range(0, lineLength):
-            add(x, y)
+            #add(x, y)
             x += 1
-            if angleTot>0.5:
-                angleTot += angle
-            else:
-                angleTot += angle
-                if angleTot > 0.5:
-                    y+=1
-            if angleTot >1:
-                angleTot-=1
-            print(angleTot)
+            angleTot    += angle
+            if angleTot > 0.5 and hasGone == False:
+                y += 1
+                hasGone = True
+            if angleTot >= 1:
+                angleTot -= 1
+                hasGone = False
+            print("AngleTot: ", angleTot, "hasGone: ", hasGone)
+            add(x, y)
 
-
-
-    for x in range (0, lineLength):
-        print()
-    print(angle)
-    print (dirx)
-    print (diry)
-
-rasterline(0 , 0 , 5 , 5)
-
+rasterline(0, 0, 10, 3)
 g.draw()
